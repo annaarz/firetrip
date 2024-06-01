1c27b3d6d7bf222bdee2641fe711bbc7829c9073
annaarz <81868053+annaarz@users.noreply.github.com>
2024-06-01 23:54:37 +0300

    Add files via upload

---
+++ hotel/hotel1/hotel.js
@@ -0,0 +1,19 @@
+const acc = document.getElementsByClassName("accordion");
+const back = document.querySelector(".back");
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+back.addEventListener("click", () => {
+  window.location.href = "../../main/main.html";
+})
