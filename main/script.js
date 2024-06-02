e6090a7e755fd579f036a503347cc7339fc1b39d
annaarz <81868053+annaarz@users.noreply.github.com>
2024-06-02 15:34:10 +0300

    Update script.js

--- main/script.js
+++ main/script.js
@@ -3,9 +3,9 @@ const hotels = document.querySelectorAll(".hotel");
 hotels.forEach(hotel => {
     hotel.addEventListener("click", (e) => {
         if (e.target.classList.contains("hotel11")) {
-            window.location.href = "../hotel/hotel6/hotel6.html";
+            window.location.href = "https://annaarz.github.io/firetrip/hotel/hotel6/hotel6.html";
         } else if(e.target.classList.contains("hotel22")) {
-            window.location.href = "../hotel/hotel1/hotel.html";
+            window.location.href = "https://annaarz.github.io/firetrip/hotel/hotel1/hotel.html";
         }
     });
 });
